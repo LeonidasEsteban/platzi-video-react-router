bdc905b87ab2340da75d4d61850eb1b4034c65fc
Leonidas Esteban <leonidasesteban@gmail.com>
2018-08-09 15:57:02 -0500

    clase 15 - configurando webpack para server render

---
+++ webpack.server.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
+module.exports = (env) => {
+  const plugins = [
+    new ExtractTextPlugin("css/[name].css")
+  ]
+
+  if (env.NODE_ENV === 'production') {
+    plugins.push(
+      new CleanWebpackPlugin(['dist'], {root: __dirname})
+    )
+  }
+
+  return {
+
+    entry: {
+      "app": path.resolve(__dirname, 'src/entries/app.js'),
+      // "redux": path.resolve(__dirname, 'src/entries/redux.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'ssr/[name].js',
+      publicPath: "/",
+      chunkFilename: 'js/[id].[chunkhash].js',
+      libraryTarget: 'commonjs2',
+    },
+    devServer: {
+      port: 9000,
+    },
+    target: 'node',
+    module: {
+      rules: [
+        {
+          // test: que tipo de archivo quiero reconocer,
+          // use: que loader se va a encargar del archivo
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react', 'stage-2'],
+            }
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: true,
+                }
+              }
+            ]
+          })
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'images/[name].[hash].[ext]',
+            }
+          }
+        },
+      ]
+    },
+    plugins
+  }
+}
